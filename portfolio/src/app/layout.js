9192d3578eb51b8290473d380867cd8bc55c84e2
GrantWass <gwasserman2@huskers.unl.edu>
2025-02-17 16:25:45 -0600

    add analytics

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,7 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className="antialiased">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
